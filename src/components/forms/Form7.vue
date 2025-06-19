11cc7def05f87f46bd003d621317142c425aa58a
Lscys <porta2910@gmail.com>
2025-06-19 11:49:06 -0500

    update forms 4, 5, 6 and 7

--- src/components/forms/Form7.vue
+++ src/components/forms/Form7.vue
@@ -74,37 +74,27 @@
 
         <hr />
 
-        <!-- <h2 class="text-xl font-semibold mb-1">Vacunación contra COVID-19</h2>
+        <h2 class="text-xl font-semibold mb-1">Vacunación contra Neumococo </h2>
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
             <div class="space-y-2">
-                <label class="block font-semibold text-sm text-gray-700">Última dosis Administrada *</label>
-                <select v-model="movNoVacunación" class="w-full border rounded p-2 text-sm">
-                    <option value="">Seleccione una opción</option>
-                    <option value="1"> 1ra. Dosis </option>
-                    <option value="2"> 2da. Dosis </option>
-                    <option value="3"> 3ra. Dosis </option>
-                    <option value="4"> Refuerzo </option>
-                </select>       
-            </div>
-            <div class="space-y-2">
-                <label class="block font-semibold text-sm text-gray-700">Fecha de última dosis *</label>
-                    <input v-model="bacAniCirculacion" type="date" class="w-full border rounded p-2 text-sm" />
+                <label class="block font-semibold text-sm text-gray-700">Fecha de Vacunación *</label>
+                    <input v-model="fcVacunacion" type="date" class="w-full border rounded p-2 text-sm" />
             </div>
             <div class="space-y-2">
                 <label class="block font-semibold text-sm text-gray-700">Motivo de no Vacunación *</label>
-                <select v-model="movNoVacunación" class="w-full border rounded p-2 text-sm">
+                <select v-model="mtvNoVacunacion" class="w-full border rounded p-2 text-sm">
                     <option value="">Seleccione una opción</option>
-                    <option value="1"> No aceptó la vacunación </option>
-                    <option value="2"> Fallecio </option>
-                    <option value="3"> Está hospitalizado </option>
-                    <option value="4"> Cursa con proceso infeccioso</option>
-                    <option value="5"> Tiene diagnóstico covid-19 </option>
-                    <option value="6"> Tiene antecedente de vanucación reciente </option>
-                    <option value="7"> Cambió de unidad </option>
-                    <option value="8"> No corresponde </option>
+                    <option value="1">No aceptó la vacunación</option>
+                    <option value="2">Falleció</option>
+                    <option value="3">Está hospitalizado</option>
+                    <option value="4">Cursa con proceso infeccioso</option>
+                    <option value="5">Tiene diagnostico COVID-19</option>
+                    <option value="6">Tiene antecedente de vacunación reciente</option>
+                    <option value="7">Cambio de unidad</option>
+                    <option value="8">No corresponde </option>
                 </select>
             </div>
-        </div> -->
+        </div>
 
         <button class="w-full bg-black text-white py-2 rounded hover:bg-gray-900">
             💾 Guardar Unidad Actual
@@ -112,7 +102,7 @@
     </div>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue';
 
 const red = ref('');
@@ -120,18 +110,9 @@ const turno = ref('');
 const frecuencia = ref('');
 const ultimaDosisAdm = ref('')
 const movNoVacunación = ref('');
+const mtvNoVacunacion = ref('')
 const fechaUltimaDosis = ref(''); 
+const fcVacunacion = ref('')
 
-export default {
-    setup() {
-        return {
-            red,
-            turno,
-            frecuencia,
-            ultimaDosisAdm,
-            movNoVacunación,
-            fechaUltimaDosis,
-        };
-    },
-};
+
 </script>
