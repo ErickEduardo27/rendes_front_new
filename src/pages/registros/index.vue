689c40968b4abbb1eecb48b7057814744fb74e9b
cnsr.sistemas@essalud.gob.pe <cnsr.sistemas@essalud.gob.pe>
2025-06-24 14:26:28 -0500

    Se agrega cambios

--- src/pages/registros/index.vue
+++ src/pages/registros/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="p-6 bg-gray-50 flex flex-col min-h-full">
 
-    <router-link to="/" class="mb-4 flex items-center gap-2 text-gray-700 hover:underline">
+    <!-- <router-link to="/" class="mb-4 flex items-center gap-2 text-gray-700 hover:underline">
       <ArrowUturnLeftIcon class="w-6 h-6" />
       Volver al Inicio
-    </router-link>
+    </router-link> -->
 
     <h1 class="text-2xl font-bold mb-1">Expediente Médico</h1>
     <p class="text-gray-600 mb-6">Información completa del paciente y formularios médicos</p>
